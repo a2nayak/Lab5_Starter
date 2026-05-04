1c4ffb636dbf376853dc65f9028b7f1be0de35e4
Anirudh Nayak <anirudhsnayak@gmail.com>
2026-05-04 13:26:25 -0700

    added unit tests

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Make sure you make a PR to your own repo's main and not the class' repo!! Otherwise you will lose points!!
 
 Name: Anirudh Nayak
+
+1) No, I would not use a unit test to test the "message" feature of a messaging application. This is because the entire messaging feature is too large to fit within the scope of a unit test. Unit tests should test small elements of one's code, which lets tests run faster and allows one to identify the specific causes of a breaking build rather than simply acknowledging that the build is broken. 
+
+2) Yes, I would use a unit test to test the "max message length" feature of a messaging application. This is because the behavior of this feature is simple and clearly defined, being small enough to place inside a unit test. Furthermore, the failure of this test helps developers narrow bugs down to only a few possible locations.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -8,4 +8,93 @@ import {
   isHexColor,
 } from '../code-to-unit-test/unit-test-me';
 
-// TODO - Part 2
+// isPhoneNumber tests
+
+test('858-141-9999 is a valid phone number', () => {
+  expect(isPhoneNumber("858-141-9999")).toBe(true);
+});
+
+test('101-000-0410 is a valid phone number', () => {
+  expect(isPhoneNumber("101-000-0410")).toBe(true);
+});
+
+test('random_string is not a valid phone number', () => {
+  expect(isPhoneNumber("random_string")).toBe(false);
+});
+
+test('1-2-3 is not a valid phone number', () => {
+  expect(isPhoneNumber("1-2-3")).toBe(false);
+});
+
+//isEmail tests
+
+test('hi_idk@gmail.com is a valid email', () => {
+  expect(isEmail("hi_idk@gmail.com")).toBe(true);
+});
+
+test('ucsd_student@ucsd.edu is a valid email', () => {
+  expect(isEmail("ucsd_student@ucsd.edu")).toBe(true);
+});
+
+test('@light_beam is not a valid email', () => {
+  expect(isEmail("@light_beam")).toBe(false);
+});
+
+test('The empty string is not a valid email', () => {
+  expect(isEmail("")).toBe(false);
+});
+
+//isStrongPassword tests
+
+test('cool_ice1999 is a strong password', () => {
+  expect(isStrongPassword("cool_ice1999")).toBe(true);
+});
+
+test('password is a strong password', () => {
+  expect(isStrongPassword("password")).toBe(true);
+});
+
+test('aj8af#h80h#1rub0$_bcasfajr is not a strong password', () => {
+  expect(isStrongPassword("aj8af#h80h#1rub0$_bcasfajr")).toBe(false);
+});
+
+test('999999 is not a strong password', () => {
+  expect(isStrongPassword("999999")).toBe(false);
+});
+
+//isDate tests
+
+test('01/01/2000 is a valid date', () => {
+  expect(isDate("01/01/2000")).toBe(true);
+});
+
+
+test('02 / 01 / 2026 is a valid date', () => {
+  expect(isDate("02/01/2026")).toBe(true);
+});
+
+test('200 / 200 / 2000 is an invalid date', () => {
+  expect(isDate("200 / 200 / 2000")).toBe(false);
+});
+
+test('2026/1/1 is an invalid date', () => {
+  expect(isDate("2026/1/1")).toBe(false);
+});
+
+//isHexColor tests
+
+test('#afafaf is a valid hex color', () => {
+  expect(isHexColor("#afafaf")).toBe(true);
+});
+
+test('#b2b is a valid hex color', () => {
+  expect(isHexColor("#b2b")).toBe(true);
+});
+
+test('#afaezf is an invalid hex color', () => {
+  expect(isHexColor("#afaezf")).toBe(false);
+});
+
+test('#e9 is an invalid hex color', () => {
+  expect(isHexColor("#a9")).toBe(false);
+});
